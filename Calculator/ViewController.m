872ae10a1d27c9b34d010a35079407451d69be1e
Eric Rosenberg <ehaydenr@gmail.com>
2014-05-14 11:49:01 -0500

    Initial Commit

---
+++ Calculator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Calculator
+//
+//  Created by Eric Rosenberg on 5/14/14.
+//  Copyright (c) 2014 ehaydenr. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
